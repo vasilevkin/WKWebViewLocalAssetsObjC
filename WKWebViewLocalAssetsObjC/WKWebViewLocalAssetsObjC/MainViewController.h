bd3ed6c666da4af0cd5047235c1d9a67c0821097
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-09 13:54:32 +0300

    Add MainViewController and instantiate it in AppDelegate.

---
+++ WKWebViewLocalAssetsObjC/WKWebViewLocalAssetsObjC/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  WKWebViewLocalAssetsObjC
+//
+//  Created by Sergey Vasilevkin on 09/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WKWebViewLocalAssetsObjC/WKWebViewLocalAssetsObjC/MainViewController.m
@@ -0,0 +1,32 @@
+//
+//  MainViewController.m
+//  WKWebViewLocalAssetsObjC
+//
+//  Created by Sergey Vasilevkin on 09/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
