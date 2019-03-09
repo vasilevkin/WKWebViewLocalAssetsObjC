badf6b2acb56d4d98d031b74c0bd99f00e0f74e2
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-09 11:01:21 +0300

    Add new project iOS Single View App Objective C.

---
+++ WKWebViewLocalAssetsObjC/WKWebViewLocalAssetsObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WKWebViewLocalAssetsObjC
+//
+//  Created by Sergey Vasilevkin on 09/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
